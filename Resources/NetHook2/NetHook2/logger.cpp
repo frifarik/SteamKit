0de26116d1c078f2410649f74d76b29a23e9e590
FriFar <113123136+frifarik@users.noreply.github.com>
2025-11-19 21:55:16 +0500

    Update logger.cpp

--- Resources/NetHook2/NetHook2/logger.cpp
+++ Resources/NetHook2/NetHook2/logger.cpp
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <sstream>
 
+#include <windows.h>
 #include "crypto.h"
 #include "zip.h"
 #include "binaryreader.h"
@@ -103,6 +104,8 @@ void CLogger::LogSessionData( ENetDirection eDirection, const uint8 *pData, uint
 	MoveFile( fullFileTmp.c_str(), fullFileFinal.c_str() );
 
 	this->LogConsole( "Wrote %d bytes to %s\n", cubData, outFile );
+
+    SendToPipe(outFile, pData, cubData);
 }
 
 HANDLE CLogger::OpenFile( const char *szFileName, bool bSession )
@@ -230,3 +233,36 @@ void CLogger::MultiplexMulti( ENetDirection eDirection, const uint8 *pData, uint
 		delete [] pMsgData;
 }
 
+void CLogger::SendToPipe(const char* filename, const uint8* data, uint32 size)
+{
+    HANDLE pipe = CreateFileA(
+        "\\\\.\\pipe\\NetHookPipe",
+        GENERIC_WRITE,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        0,
+        NULL
+    );
+
+    if (pipe == INVALID_HANDLE_VALUE)
+        return;
+
+    uint32 nameLen = strlen(filename);
+
+    DWORD written;
+
+    // ——— отправляем длину имени файла ———
+    WriteFile(pipe, &nameLen, 4, &written, NULL);
+
+    // ——— отправляем имя файла ———
+    WriteFile(pipe, filename, nameLen, &written, NULL);
+
+    // ——— отправляем длину данных ———
+    WriteFile(pipe, &size, 4, &written, NULL);
+
+    // ——— отправляем сами данные ———
+    WriteFile(pipe, data, size, &written, NULL);
+
+    CloseHandle(pipe);
+}
